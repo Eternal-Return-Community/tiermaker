bc48022ba91d9cb222242f9538f549421f567935
nicaksks <juliesuzuya@outlook.com>
2024-10-06 09:55:01 -0300

    .

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,11 +1,13 @@
 import leaderboard from './leaderboard.js';
 
-const characterPool = async () => {
+let isDragging = false;
+let scrollInterval;
 
+const characterPool = async () => {
     const leaderboards = await leaderboard()
     const pool = document.getElementById('character-pool');
 
-    for (const { nickname, elo, character } of leaderboards) {
+    for (const { nickname, rank, elo, character } of leaderboards) {
         pool.innerHTML += `
         <div class='player-info' draggable="true" data-player=${nickname}>
             <span class='player-name'>${nickname}</span>
@@ -22,6 +24,7 @@ document.addEventListener("dragstart", event => {
     if (target) {
         event.dataTransfer.setData("Player", target.dataset.player);
         target.style.opacity = '0.2';
+        isDragging = true;
     }
 });
 
@@ -29,20 +32,57 @@ document.addEventListener("dragend", event => {
     const target = event.target.closest('.player-info');
     if (target) {
         target.style.opacity = '1';
+        isDragging = false;
+        stopScrolling();
     }
 });
 
 document.addEventListener("dragover", event => {
     event.preventDefault();
+    if (isDragging) {
+        const scrollThreshold = 50;
+        const scrollSpeed = 5;
+
+        const { clientY } = event;
+        const { innerHeight } = window;
+
+        if (clientY < scrollThreshold) {
+            startScrolling(-scrollSpeed);
+        } else if (clientY > innerHeight - scrollThreshold) {
+            startScrolling(scrollSpeed);
+        } else {
+            stopScrolling();
+        }
+    }
 });
 
 document.addEventListener("drop", event => {
     event.preventDefault();
+    isDragging = false;
+    stopScrolling();
 
     const data = event.dataTransfer.getData("Player");
     const playerElement = document.querySelector(`[data-player='${data}']`);
-    event.target.closest('.tier').appendChild(playerElement); 
+    const targetTier = event.target.closest('.tier');
+    if (targetTier && playerElement) {
+        targetTier.appendChild(playerElement);
+    }
 });
+
+function startScrolling(speed) {
+    if (!scrollInterval) {
+        scrollInterval = setInterval(() => {
+            window.scrollBy(0, speed);
+        }, 16); 
+    }
+}
+
+function stopScrolling() {
+    if (scrollInterval) {
+        clearInterval(scrollInterval);
+        scrollInterval = null;
+    }
+}
 
 const disableLoading = () => {
     document.querySelector('body').removeAttribute('id')
